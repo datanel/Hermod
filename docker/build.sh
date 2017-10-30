5fa4ab468658866390effedba377aade5ac5d701
Rémy Abi-Khalil <remyabi@gmail.com>
2017-10-30 16:02:38 +0100

    Docker hub moved in the official.

--- docker/build.sh
+++ docker/build.sh
@@ -3,12 +3,12 @@
 set -ex
 
 # You should be on origin/release
-VERSION=$(git describe --tags --exact-match)
+VERSION=$(git describe --abbrev=0 --tags)
 
 if [ ! -f 'docker/config.env' ]; then
     echo "'docker/config.env' environment file is missing. Please create it, see docker/config.env.dist for example"
     exit 66 # EX_NOINPUT
 fi
 
-docker build --build-arg VERSION=${VERSION} --rm -t hermod_php:${VERSION} -f docker/php/Dockerfile .
-docker build --rm -t hermod_nginx:${VERSION} -f docker/nginx/Dockerfile .
+docker build --build-arg VERSION=${VERSION} --rm -t kisiodigital/hermod_php:${VERSION} -f docker/php/Dockerfile .
+docker build --rm -t kisiodigital/hermod_nginx:${VERSION} -f docker/nginx/Dockerfile .
