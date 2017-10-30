4738dcec2817b8a0fdb93d20abf2a46f8f54aeaa
Rémy Abi-Khalil <remyabi@gmail.com>
2017-10-30 14:47:49 +0100

    Docker hub moved in the official.

--- docker/build.sh
+++ docker/build.sh
@@ -10,5 +10,5 @@ if [ ! -f 'docker/config.env' ]; then
     exit 66 # EX_NOINPUT
 fi
 
-docker build --build-arg VERSION=${VERSION} --rm -t ${DOCKER_REGISTRY_HOST}/hermod_php:${VERSION} -f docker/php/Dockerfile .
-docker build --rm -t ${DOCKER_REGISTRY_HOST}/hermod_nginx:${VERSION} -f docker/nginx/Dockerfile .
+docker build --build-arg VERSION=${VERSION} --rm -t hermod_php:${VERSION} -f docker/php/Dockerfile .
+docker build --rm -t hermod_nginx:${VERSION} -f docker/nginx/Dockerfile .
